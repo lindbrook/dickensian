d5e9aa43c38b45c1d5640f968ce6b35b5b02cc3b
lindbrook <lindbrook@gmail.com>
2018-04-04 10:38:46 -0700

    add dickens.utf file

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Gutenberg text file encoding.
+#'
+#' Type of text encoding used for Dickens's works. Used to determine file path for download.
+#'
+#' @format A data frame with two variables.
+#'  \describe{
+#'      \item{\code{ID}}{Gutenberg ID}
+#'      \item{\code{utf8}}{Indicator of utf8 encoding}
+#'  }
+#' @docType data
+"dickens.utf"
