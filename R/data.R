cea1f1a1cf0b93349c069650691cdc3ffd0f5d6b
lindbrook <lindbrook@gmail.com>
2018-04-05 22:19:16 -0700

    replace "dickens.utf" with "bibliography"

--- R/data.R
+++ R/data.R
@@ -1,11 +1,15 @@
-#' Gutenberg text file encoding.
+#' Data bibliography.
 #'
 #' Type of text encoding used for Dickens's works. Used to determine file path for download.
 #'
-#' @format A data frame with two variables.
+#' @format A data frame with six variables.
 #'  \describe{
+#'      \item{\code{obs}}{ID}
 #'      \item{\code{ID}}{Gutenberg ID}
 #'      \item{\code{utf8}}{Indicator of utf8 encoding}
+#'      \item{\code{Title}}{Raw title}
+#'      \item{\code{Title2}}{Clean title}
+#'      \item{\code{Year_Comp}}{Year completed}
 #'  }
 #' @docType data
-"dickens.utf"
+"bibliography"
