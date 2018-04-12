3b5c174e48fa07bcb2ee8d578bc9db7f39382fb6
lindbrook <lindbrook@gmail.com>
2018-04-12 10:45:11 -0700

    add getWork2() prototype

--- R/getWork.R
+++ R/getWork.R
@@ -39,3 +39,32 @@ getWork <- function(id, first, last) {
   end <- stringi::stri_locate_last_fixed(data_character, last)[, "end"]
   stri_sub(data_character, start, end)
 }
+
+
+#' Get Work.
+#'
+#' Fetch work" from gutenberg.org for 'qunateda'.
+#' @param id Numeric. Gutenberg ID.
+#' @return quanteda object
+#' @export
+#' @examples
+#' # A Christmas Carol:
+#' getWork(46, "PREFACE", "One")
+#'
+#' # A Tale of Two Cities:
+#' getWork(98, "CONTENTS", "known")
+
+getWork2 <- function(id) {
+  url <- dickensian::gutenbergURL(id)
+  data_character <- quanteda::texts(readtext::readtext(url))
+
+  title <- dickensian::bibliography[dickensian::bibliography$ID == id, "gTitle"]
+  first <- paste0(toupper(title), "\n")
+
+  last <- dickensian::bibliography[dickensian::bibliography$ID == id,
+    "last_word"]
+
+  start <- stringi::stri_locate_first_fixed(data_character, first)[, "start"]
+  end <- stringi::stri_locate_last_fixed(data_character, last)[, "end"]
+  stri_sub(data_character, start, end)
+}
