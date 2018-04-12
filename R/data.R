a2446e9ffb5b65f6955e39f9812541e0458c15f6
lindbrook <lindbrook@gmail.com>
2018-04-12 10:48:01 -0700

    test new bibliography

--- R/data.R
+++ R/data.R
@@ -8,8 +8,10 @@
 #'      \item{\code{ID}}{Gutenberg ID}
 #'      \item{\code{utf8}}{Indicator of utf8 encoding}
 #'      \item{\code{Title}}{Raw title}
-#'      \item{\code{Title2}}{Clean title}
+#'      \item{\code{gTitle}}{Gutenberg title}
 #'      \item{\code{Year_Comp}}{Year completed}
+#'      \item{\code{first_word}}{first word}
+#'      \item{\code{last_word}}{last word}
 #'  }
 #' @docType data
 "bibliography"
