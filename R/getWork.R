63b7ca278b028f72b57a509f031bb4c0238d10a6
lindbrook <lindbrook@gmail.com>
2018-04-23 09:24:54 -0700

    test getWork()

--- R/getWork.R
+++ R/getWork.R
@@ -1,46 +1,29 @@
-#' Gutenberg URL
+#' Download work from gutenberg.org
 #'
-#' Get URL for download.
+#' Gets txt file.
 #' @param id Numeric. Gutenberg ID of work.
-#' @return Character. URL as text string.
+#' @param g_title Character. Start of text (gutenberg title).
+#' @param last_text Character. End of text (last word, include punctuation).
+#' @return quanteda object.
 #' @export
 
-gutenbergURL <- function(id) {
+getWork <- function(id, g_title, last_text) {
   if (is.numeric(id) == FALSE) {
     stop('"id" must be numeric')
   }
 
-  if (dickensian::bibliography[dickensian::bibliography$id == id, "utf8"]) {
-    paste0("http://www.gutenberg.org/ebooks/", id, ".txt.utf-8")
-  } else {
-    paste0("http://www.gutenberg.org/files/", id, "/", id, "-0.txt")
+  if (is.character(g_title) == FALSE) {
+    stop('"g_title" must be text string.')
   }
-}
 
-#' Get Work.
-#'
-#' Fetch work" from gutenberg.org for 'qunateda'.
-#' @param id Numeric. Gutenberg ID.
-#' @return quanteda object
-#' @export
-#' @examples
-#' # A Christmas Carol:
-#' getWork(46)
-#'
-#' # A Tale of Two Cities:
-#' getWork(98)
+  if (is.character(last_text) == FALSE) {
+    stop('"last_text" must be text string.')
+  }
 
-getWork <- function(id) {
-  url <- dickensian::gutenbergURL(id)
+  url <- paste0("http://www.gutenberg.org/cache/epub/", id, "/pg", id, ".txt")
   data_character <- quanteda::texts(readtext::readtext(url))
-
-  title <- dickensian::bibliography[dickensian::bibliography$id == id, "gTitle"]
-  first <- paste0(toupper(title), "\n")
-
-  last <- dickensian::bibliography[dickensian::bibliography$id == id,
-    "last_word"]
-
+  first <- paste0(toupper(g_title), "\n")
   start <- stringi::stri_locate_first_fixed(data_character, first)[, "start"]
-  end <- stringi::stri_locate_last_fixed(data_character, last)[, "end"]
+  end <- stringi::stri_locate_last_fixed(data_character, last_text)[, "end"]
   stringi::stri_sub(data_character, start, end)
 }
